31fc77356a35a2a28510155a9c34a9ab7acdef0a
Moises Victoria <moisesvc1815@gmail.com>
2022-12-01 19:53:35 -0800

    commit tree dac5153879d3c7a31884352a6f205c86f28e30e4

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
-
 // Write your code here
+var num1 =62
+var num2 =1
+var multiply = num1 * num2
+
+var random = Math.floor(Math.random()*10)
+
+var num3 = 6
+var num4 = 22
+var mod = num4 % num3
+
+var max = Math.max(1, 3, 20)
